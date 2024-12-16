da002fa854af6e995b3eaad6539ceb57c5c31a10
nathanpage <nathan.page@eonx.com>
2024-12-16 14:29:51 +1100

    Most basic working setup for API

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 use App\Infrastructure\Symfony\HttpKernel\ApplicationKernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once \dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new ApplicationKernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
